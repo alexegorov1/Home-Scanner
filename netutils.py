00bde6f1155ac79c9cdce37dcd23467e598db746
chmod777Alex <59450257+alexegorov1@users.noreply.github.com>
2025-04-16 00:05:06 -0400

    Update netutils.py

--- netutils.py
+++ netutils.py
@@ -9,11 +9,19 @@ def reverse_dns(ip: str, timeout: float = 1.0) -> Optional[str]:
         return None
     try:
         ipaddress.ip_address(ip)
-        prev_timeout = socket.getdefaulttimeout()
-        socket.setdefaulttimeout(timeout)
-        try:
+        with _temporary_socket_timeout(timeout):
             return socket.gethostbyaddr(ip)[0]
-        finally:
-            socket.setdefaulttimeout(prev_timeout)
     except (ValueError, socket.herror, socket.gaierror, OSError):
         return None
+
+
+class _temporary_socket_timeout:
+    def __init__(self, timeout: float):
+        self.new_timeout = timeout
+        self.prev_timeout = socket.getdefaulttimeout()
+
+    def __enter__(self):
+        socket.setdefaulttimeout(self.new_timeout)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        socket.setdefaulttimeout(self.prev_timeout)
