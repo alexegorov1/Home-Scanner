5ec18c81b1987dba3f86f6435136a030b81d1d79
chmod777Alex <59450257+alexegorov1@users.noreply.github.com>
2025-04-17 10:59:20 -0400

    Update portsweep.py

--- portsweep.py
+++ portsweep.py
@@ -3,13 +3,15 @@ from typing import List
 
 
 def sweep_host_ports(ip: str, ports: List[int], timeout: float = 0.5) -> List[int]:
-    return [port for port in ports if is_port_open(ip, port, timeout)]
+    return [port for port in ports if _port_open(ip, port, timeout)]
 
 
-def is_port_open(ip: str, port: int, timeout: float) -> bool:
+def _port_open(ip: str, port: int, timeout: float) -> bool:
     try:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            sock.settimeout(timeout)
-            return sock.connect_ex((ip, port)) == 0
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(timeout)
+        result = sock.connect_ex((ip, port)) == 0
+        sock.close()
+        return result
     except OSError:
         return False
