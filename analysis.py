b22800d1af9619da35facbc9d6add180dad636c8
chmod777Alex <59450257+alexegorov1@users.noreply.github.com>
2025-05-07 21:49:13 -0400

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -18,16 +18,16 @@ class LogAnalyzer:
             f"[{rule.get('title', 'Unnamed Rule')}] {line.strip()}"
             for line in logs
             for rule in self.rules
-            if self._match(rule.get("detection", {}).get("selection", {}), line.lower())
+            if self._match(rule.get("detection", {}).get("selection", {}), line.strip().lower())
         ]
 
     def _match(self, selection, text):
         try:
             return any(
-                re.search(re.escape(val), text, re.IGNORECASE)
+                re.search(re.escape(item), text, re.IGNORECASE)
                 for val in selection.values()
-                if isinstance(val, str) or isinstance(val, list)
-                for val in (val if isinstance(val, list) else [val])
+                for item in (val if isinstance(val, list) else [val])
+                if isinstance(item, str)
             )
         except:
             return False
