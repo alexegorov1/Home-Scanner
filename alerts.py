756375e39f99622e24a60610b75b70db029356a8
chmod777Alex <59450257+alexegorov1@users.noreply.github.com>
2025-05-26 23:29:00 -0400

    Update alerts.py

--- alerts.py
+++ alerts.py
@@ -40,6 +40,8 @@ class AlertManager:
             self.logger.warning("Skipped sending alert: SMTP config incomplete or disabled.")
             return
 
+        self._send_email(f"[{severity.upper()}] {source}", message)
+
     def _send_email(self, subject, body):
         msg = MIMEMultipart()
         msg["From"] = self.email_from
