15f0736421e52a622d06b8241bd6baa5dd7eb659
chmod777Alex <59450257+alexegorov1@users.noreply.github.com>
2025-06-23 20:32:55 -0400

    Update cli.py

--- cli.py
+++ cli.py
@@ -30,10 +30,6 @@ class HomescannerCLI:
             self.print_status()
         elif self.args.command == "uptime":
             self.print_uptime()
-        elif self.args.command == "disk":
-            self.check_disk()
-        elif self.args.command == "logs":
-            self.show_logs()
         elif self.args.command == "incidents":
             self.show_incidents()
         elif self.args.command == "scan":
